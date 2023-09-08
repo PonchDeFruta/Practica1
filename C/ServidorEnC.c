77d5192ead935fb00817aadca5264682ca35d368
Abdiel Castro <di.cast.0120@gmail.com>
2023-09-07 22:29:23 -0600

    ServidorEnC.c : Medio codigo

---
+++ C/ServidorEnC.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    char buffer[1024];
+
+    server_socket = socket(AF_INET, SOCK_STEAM, 0);
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(12345);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+}
