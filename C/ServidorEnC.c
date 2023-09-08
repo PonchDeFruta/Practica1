fc3193857c19a92419d6d4e1d0836beaae4f61b1
Cesar Romero Garcia <cesarromerogarcia4@gmail.com>
2023-09-07 23:02:58 -0600

    ServidorEnC: codigo copiado

--- C/ServidorEnC.c
+++ C/ServidorEnC.c
@@ -10,10 +10,21 @@ int main() {
     socklen_t client_len = sizeof(client_addr);
     char buffer[1024];
 
-    server_socket = socket(AF_INET, SOCK_STEAM, 0);
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(12345);
     server_addr.sin_addr.s_addr = INADDR_ANY;
+
+
+
+bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+listen(server_socket, 1);
+printf("Servidor C esperando conexiones... \n");
+client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
+printf("Cliente conectad: %s \n", inet_ntoa(client_addr.sin_addr));
+close(server_socket);
+
+return 0;
 }
